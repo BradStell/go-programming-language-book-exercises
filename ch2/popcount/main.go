d66bf7eef741723e2c4c0704393d3e77a5ffb48c
Brad Stell <bradstell87@gmail.com>
2018-06-05 18:35:45 -0400

    adding popcount

---
+++ ch2/popcount/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// pc[i] is the population count of i
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func main() {
+	n, err := strconv.ParseUint(os.Args[1], 10, 64)
+	if err != nil {
+		fmt.Printf("error %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%d = %b = %d\n", n, n, PopCount(n))
+
+	fmt.Printf("%v\n", 3&2)
+}
